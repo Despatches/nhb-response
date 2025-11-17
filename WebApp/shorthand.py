e4d41d63479002bd2c3805037addb678d7fb8716
Nelsonh <Humes101@gmail.com>
2025-11-17 04:13:14 +0000

    start to flesh out injection names

---
+++ WebApp/shorthand.py
@@ -0,0 +1,9 @@
+shorts = {
+	"MM" : "Mansell Mctaggart",
+	"FF" : "Furnace Farm House",
+	"FFO" : "Furnace Farmhouse Owner",
+	"MMAM" : "Momentum Asset Management",
+	"NHB" : "NHB Investments Limited",
+	"NatHB": "National Homebuyers",
+
+}
