11b36b91632d0e8d201213f8e2ca895d43d152eb
dpost <Despatches@gmail.com>
2025-11-17 04:33:05 +0000

    new section : NHB Disclosure . Intended to ID significant defective disclosure,  (defective marketing by the Agent should be in a separate section, Marketing or maybe Misrepresentation.

--- WebApp/__init__.py
+++ WebApp/__init__.py
@@ -51,6 +51,12 @@ def create_app():
                     },
                 ]
             },
+             {
+                "label" :"NHB Disclosure",
+                "id" : "disclosure_nhb",
+                "dynamic" : "sections/disclosure_nhb.jinja",
+                #"lit_exhibit" : True,
+            },
             {
                 "label" :"Boundaries",
                 "id" : "boundary",
