4f6dd89f246c6f2a204891f5030db5c2c2cdf13a
dpost <Despatches@gmail.com>
2025-12-20 18:30:09 +0000

    defamation act correte path in-it

--- WebApp/__init__.py
+++ WebApp/__init__.py
@@ -222,7 +222,7 @@ def create_app():
             {
                 "id": "defamation_act_sec.5_web_operators",
                 "label": "Defamation Web Operators",
-                "dynamic" : "sections/defamation_act_sec.5_web_operators.jinja",
+                "dynamic" : "defamations_act/defamation_act_sec.5_web_operators.jinja",
                 "subsections": []
             },
             {
